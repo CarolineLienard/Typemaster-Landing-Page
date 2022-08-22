0b31c6e284651f3c78c3f8e0e80c91d6c3fe11d1
Caroline Lienard <carolinemrd@outlook.fr>
2022-08-22 21:39:22 +0200

    ♻️ Refactoring

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,23 @@
 module.exports = {
   content: ["./*.{html,js}"],
   theme: {
-    extend: {},
+    screens: {
+      sm: '750px',
+      md: '750px',
+      lg: '950px',
+      xl: '1190px',
+    },
+    colors: {
+      'blue': '#1fb6ff',
+      'purple': '#7e5bef',
+      'pink': '#ff49db',
+      'orange': '#ff7849',
+      'green': '#13ce66',
+      'yellow': '#ffc82c',
+      'gray-dark': '#273444',
+      'gray': '#8492a6',
+      'gray-light': '#d3dce6',
+    },
   },
   plugins: [],
 }
